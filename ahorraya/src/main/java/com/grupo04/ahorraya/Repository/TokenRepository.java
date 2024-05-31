def277ce4ec3a9e99d5ee0e8ec5879510135fc28
GerardoHqz <gerardohenriquez.sv@gmail.com>
2024-05-31 01:45:30 -0600

    try add postgis

--- ahorraya/src/main/java/com/grupo04/ahorraya/Repository/TokenRepository.java
+++ ahorraya/src/main/java/com/grupo04/ahorraya/Repository/TokenRepository.java
@@ -3,10 +3,12 @@ package com.grupo04.ahorraya.Repository;
 import com.grupo04.ahorraya.models.entities.Token;
 import com.grupo04.ahorraya.models.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.UUID;
 
+@Repository
 public interface TokenRepository extends JpaRepository<Token, UUID> {
     List<Token> findByUserAndActive(User user, Boolean active);
 }
--- ahorraya/src/main/java/com/grupo04/ahorraya/Repository/UserRepository.java
+++ ahorraya/src/main/java/com/grupo04/ahorraya/Repository/UserRepository.java
@@ -2,9 +2,11 @@ package com.grupo04.ahorraya.Repository;
 
 import com.grupo04.ahorraya.models.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
     User findByUsername(String email);
 }
