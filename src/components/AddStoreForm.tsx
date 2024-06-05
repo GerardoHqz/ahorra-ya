468728f4ff7329652dbb97e78d769759aa4e6e83
alejandra-villalobos <00132820@uca.edu.sv>
2024-06-05 14:18:06 -0600

    ✨ Added: Create store

--- src/components/AddStoreForm.tsx
+++ src/components/AddStoreForm.tsx
@@ -1,76 +1,59 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Store } from "../interfaces/Stores";
-import { Button, Input, Modal } from "antd";
+import { Button, Form, Input, Modal, Select } from "antd";
 import { IoIosLink } from "react-icons/io";
 import { FiPhone, FiUser } from "react-icons/fi";
 import { AiOutlineMail } from "react-icons/ai";
+import { getAllDepartmentsService } from "../api/departments";
+import { getAllMunicipalitiesService } from "../api/municipalities";
+import { Department } from "../interfaces/Departments";
+import { Municipality } from "../interfaces/Municipalities";
+import { createStoreService } from "../api/stores";
+import { ToastContainer } from "react-toastify";
 
 type AddStoreFormProps = {
   open: boolean;
   setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  latitude: number;
+  longitude: number;
 };
 
-const AddStoreForm = ({ open, setOpen }: AddStoreFormProps) => {
-  const [formData, setFormData] = useState<Store>({
-    id: "",
-    name: "",
-    description: "",
-    department: "",
-    town: "",
-    address: "",
-    owner_name: "",
-    website: "",
-    phone: "",
-    email: "",
-  });
+const AddStoreForm = ({
+  open,
+  setOpen,
+  latitude,
+  longitude,
+}: AddStoreFormProps) => {
+  const [departments, setDepartments] = useState<Department[]>([]);
+  const [municipalities, setMunicipalities] = useState<Municipality[]>([]);
 
-  const handleChange = (
-    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
-  ) => {
-    const { name, value } = e.target;
-    setFormData({ ...formData, [name]: value });
-  };
+  useEffect(() => {
+    getAllDepartmentsService(
+      "eyJhbGciOiJIUzM4NCJ9.eyJzdWIiOiJhbGVAdGVzdC5jb20iLCJpYXQiOjE3MTc1NjM5MTAsImV4cCI6MTcxODg1OTkxMH0.oSJa6e8I6DLqmqAYVmLlu-RKM7921Wzv3DmjSYWMoGbxcpCODQEhWhuwykGGs2yi"
+    ).then((data) => setDepartments(data));
+    getAllMunicipalitiesService(
+      "eyJhbGciOiJIUzM4NCJ9.eyJzdWIiOiJhbGVAdGVzdC5jb20iLCJpYXQiOjE3MTc1NjM5MTAsImV4cCI6MTcxODg1OTkxMH0.oSJa6e8I6DLqmqAYVmLlu-RKM7921Wzv3DmjSYWMoGbxcpCODQEhWhuwykGGs2yi"
+    ).then((data) => setMunicipalities(data));
+  }, []);
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+  const filterOption = (
+    input: string,
+    option?: { label: string; value: string }
+  ) => (option?.label ?? "").toLowerCase().includes(input.toLowerCase());
 
-    const {
-      id,
-      name,
-      description,
-      department,
-      town,
-      address,
-      owner_name,
-      website,
-      phone,
-      email,
-    } = formData;
-
-    console.log("Form data:", {
-      id,
-      name,
-      description,
-      department,
-      town,
-      address,
-      owner_name,
-      website,
-      phone,
-      email,
-    });
-    setFormData({
-      id: "",
-      name: "",
-      description: "",
-      department: "",
-      town: "",
-      address: "",
-      owner_name: "",
-      website: "",
-      phone: "",
-      email: "",
-    });
+  const handleSubmit = async (values: Store) => {
+    values.latitude = latitude;
+    values.longitude = longitude;
+    console.log(values)
+    try {
+      await createStoreService(
+        "eyJhbGciOiJIUzM4NCJ9.eyJzdWIiOiJhbGVAdGVzdC5jb20iLCJpYXQiOjE3MTc1NjM5MTAsImV4cCI6MTcxODg1OTkxMH0.oSJa6e8I6DLqmqAYVmLlu-RKM7921Wzv3DmjSYWMoGbxcpCODQEhWhuwykGGs2yi",
+        values
+      );
+      setOpen(false);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
@@ -78,117 +61,130 @@ const AddStoreForm = ({ open, setOpen }: AddStoreFormProps) => {
       title="Agregar tienda"
       style={{ top: 20 }}
       open={open}
-      onOk={() => setOpen(false)}
+      footer={[]}
       onCancel={() => setOpen(false)}
-      footer={[
-        <Button key="back" onClick={() => setOpen(false)}>
-          Return
-        </Button>,
-        <Button className="bg-gradient-to-r from-pink to-orange text-white text-center py-3 rounded-md">
-          Submit
-        </Button>,
-      ]}
     >
-      <form onSubmit={handleSubmit} className="flex flex-col gap-3">
-        <label className="flex flex-col pb-8">
-          Nombre:
-          <input
-            type="text"
-            name="name"
-            value={formData.name}
-            onChange={handleChange}
-            className="border border-secondary-text border-opacity-25 p-2 mt-2 rounded-md "
-          />
-        </label>
-        <label className="flex flex-col pb-8">
-          Descripción:
-          <textarea
-            name="description"
-            value={formData.description}
-            onChange={handleChange}
-            className="border border-secondary-text border-opacity-25 p-2 mt-2 rounded-md "
-          />
-        </label>
+      <Form
+        onFinish={handleSubmit}
+        className="flex flex-col gap-3"
+        layout="vertical"
+      >
+        <Form.Item
+          label="Nombre"
+          name="name"
+          rules={[{ required: true, message: "Por favor ingrese un nombre" }]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          label="Descripción"
+          name="description"
+          rules={[
+            { required: true, message: "Por favor ingrese una descripción" },
+          ]}
+        >
+          <Input.TextArea />
+        </Form.Item>
         <span className="flex justify-between">
-          <label className="flex flex-col pb-8">
-            Departamento:
-            <input
-              type="text"
-              name="department"
-              value={formData.department}
-              onChange={handleChange}
-              className="w-full border border-secondary-text border-opacity-25 p-2 mt-2 rounded-md "
+          <Form.Item
+            label="Departamento"
+            name="departament"
+            rules={[
+              {
+                required: true,
+                message: "Por favor seleccione un departamento",
+              },
+            ]}
+          >
+            <Select
+              showSearch
+              placeholder="Seleccione un departamento"
+              optionFilterProp="children"
+              filterOption={filterOption}
+              options={departments.map((department) => ({
+                value: department.name,
+                label: department.name,
+              }))}
             />
-          </label>
-          <label className="flex flex-col pb-8">
-            Municipio:
-            <input
-              type="text"
-              name="town"
-              value={formData.town}
-              onChange={handleChange}
-              className="w-full border border-secondary-text border-opacity-25 p-2 mt-2 rounded-md "
+          </Form.Item>
+          <Form.Item
+            label="Municipio"
+            name="municipality"
+            rules={[
+              { required: true, message: "Por favor seleccione un municipio" },
+            ]}
+          >
+            <Select
+              showSearch
+              placeholder="Seleccione un municipio"
+              optionFilterProp="children"
+              filterOption={filterOption}
+              options={municipalities.map((municipality) => ({
+                value: municipality.name,
+                label: municipality.name,
+              }))}
             />
-          </label>
+          </Form.Item>
         </span>
-        <label className="flex flex-col pb-8">
-          Dirección:
-          <input
-            type="text"
-            name="address"
-            value={formData.address}
-            onChange={handleChange}
-            className="border border-secondary-text border-opacity-25 p-2 mt-2 rounded-md "
-          />
-        </label>
+        <Form.Item
+          label="Dirección"
+          name="direction"
+          rules={[
+            { required: true, message: "Por favor ingrese una dirección" },
+          ]}
+        >
+          <Input />
+        </Form.Item>
         <h1 className="text-sm text-secondary-text py-3 mb-5">
           Información de contacto
         </h1>
-        <label className="flex flex-col pb-8">
-          Propiertario:
+        <Form.Item label="Nombre del contacto" name="ownerName">
           <Input
             type="text"
-            name="owner_name"
-            value={formData.owner_name}
-            onChange={handleChange}
+            name="ownerName"
             className="border border-secondary-text border-opacity-25 p-2 mt-2 rounded-md "
-            prefix={<FiUser size={21} color="#808080" />}
+            prefix={<FiUser size={20} color="#808080" />}
           />
-        </label>
-        <label className="flex flex-col pb-8">
-          Sitio web:
-          <Input
-            type="text"
-            name="website"
-            value={formData.website}
-            onChange={handleChange}
-            className="border border-secondary-text border-opacity-25 p-2 mt-2 rounded-md "
-            prefix={<IoIosLink size={25} color="#808080" />}
-          />
-        </label>
-        <label className="flex flex-col pb-8">
-          Número de teléfono:
+        </Form.Item>
+        <Form.Item label="Número de teléfono" name="phone">
           <Input
             type="text"
             name="phone"
-            value={formData.phone}
-            onChange={handleChange}
             className="border border-secondary-text border-opacity-25 p-2 mt-2 rounded-md "
             prefix={<FiPhone size={20} color="#808080" />}
           />
-        </label>
-        <label className="flex flex-col pb-8">
-          Correo electronico:
+        </Form.Item>
+        <Form.Item label="Correo electrónico" name="email">
           <Input
             type="text"
             name="email"
-            value={formData.email}
-            onChange={handleChange}
             className="border border-secondary-text border-opacity-25 p-2 mt-2 rounded-md "
             prefix={<AiOutlineMail size={20} color="#808080" />}
           />
-        </label>
-      </form>
+        </Form.Item>
+        <Form.Item label="Sitio web" name="website">
+          <Input
+            type="text"
+            name="website"
+            className="border border-secondary-text border-opacity-25 p-2 mt-2 rounded-md "
+            prefix={<IoIosLink size={20} color="#808080" />}
+          />
+        </Form.Item>
+        <div className="flex justify-between">
+          <button
+            onClick={() => setOpen(false)}
+            className="py-2 px-4 rounded-md border-2"
+          >
+            Regresar
+          </button>
+          <button
+            type="submit"
+            className="bg-orange-400 py-2 px-4 text-white rounded-md"
+          >
+            Guardar
+          </button>
+        </div>
+      </Form>
     </Modal>
   );
 };
--- src/pages/Map.jsx
+++ src/pages/Map.jsx
@@ -7,14 +7,14 @@ import OrangePin from "../assets/OrangePin.png";
 import BluePin from "../assets/BluePin.png";
 import { useState, useEffect } from "react";
 import AddStoreForm from "../components/AddStoreForm";
+import { ToastContainer } from "react-toastify";
 
 const icon = new Icon ({
   iconUrl: OrangePin,
   iconSize: [30, 41]
 });
 
-function AddStore({ setOpen }) {
-  const [position, setPosition] = useState(null);
+function AddStore({ setOpen, position, setPosition }) {
 
   useMapEvents({
     click(e) {
@@ -42,6 +42,7 @@ const MapComponent = ({ position }) => {
 const Map = () => {
   const [position, setPosition] = useState([13.7035233, -89.2116845]);
   const [openStoreForm, setOpenStoreForm] = useState(false);
+  const [selectedPosition, setSelectedPosition] = useState(position);
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition((position) => {
@@ -51,7 +52,8 @@ const Map = () => {
 
   return (
     <Layout className="min-h-screen text-bg-dark-blue dark:text-white">
-      <AddStoreForm open={openStoreForm} setOpen={setOpenStoreForm}/>
+      <ToastContainer />
+      <AddStoreForm open={openStoreForm} setOpen={setOpenStoreForm} latitude={selectedPosition.lat} longitude={selectedPosition.lng}/>
       <SideMenu />
       <Layout>
         <MapContainer center={position} zoom={13} style={{ height: "100vh" }}>
@@ -60,7 +62,7 @@ const Map = () => {
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
           <MapComponent position={position} />
-          <AddStore setOpen={setOpenStoreForm} />
+          <AddStore setOpen={setOpenStoreForm} setPosition={setSelectedPosition} position={selectedPosition}/>
         </MapContainer>
       </Layout>
     </Layout>
