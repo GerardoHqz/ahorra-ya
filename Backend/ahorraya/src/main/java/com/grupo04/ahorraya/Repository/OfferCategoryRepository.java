2c604971afc940e5fc97dc3fe0fb5d8086936a08
GerardoHqz <gerardohenriquez.sv@gmail.com>
2024-06-04 22:15:43 -0600

    delete files and add json file for insomnia

--- Backend/ahorraya/src/main/java/com/grupo04/ahorraya/Repository/OfferCategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.grupo04.ahorraya.Repository;
-
-import com.grupo04.ahorraya.models.entities.Offer_Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface OfferCategoryRepository extends JpaRepository<Offer_Category, UUID> {
-}
